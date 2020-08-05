80cef40a71ceddbc94dbb084cbe882c1604d7c6f
linbo20110131 <493658400@qq.com>
2020-08-05 20:40:15 +0800

    socket文件上传

--- src/OneUpload/method5/Client1.java
+++ src/OneUpload/method5/Client1.java
@@ -14,12 +14,14 @@ public class Client1 {
 	public static void main(String[] args) throws InterruptedException {
 		Client1 client1 = new Client1();
 		File file = new File("F:\\linux\\linuxIso\\CentOS-6.8-x86_64-bin-DVD1.iso");
-		client1.upload(file,"192.168.1.55",25420);
+		client1.upload(file,"192.168.1.57",25420);
+		//client1.upload(file,"192.168.1.56",25420);
+
 
 
 		Client1 client11 = new Client1();
 		File file1 = new File("F:\\linux\\linuxIso\\CentOS-7-x86_64-DVD-1511.iso");
-		client11.upload(file1,"192.168.1.55",25420);
+		//client11.upload(file1,"192.168.1.56",25420);
 
 	}
 
